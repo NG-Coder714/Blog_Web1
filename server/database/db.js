f05234ef03d11c5ecdbbe2999e941d22356c67a5
Nitesh Kumar <niteshpc39@gmail.com>
2024-12-27 22:43:32 +0530

    Initial commit

---
+++ server/database/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const Connection = async () => {
+    
+    const URL = process.env.DB_URL 
+    
+    try {
+        await mongoose.connect(URL, { useNewUrlParser: true })
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting to the database ', error);
+    }
+};
+
+export default Connection;
